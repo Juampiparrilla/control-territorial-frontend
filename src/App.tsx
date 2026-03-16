5693060036e0e73585d81f89059d850733ac89d0
Juan Pablo Parrilla <juan.pablo.parrilla@nobu.com.ar>
2026-03-16 12:29:49 -0300

    Creacion de la estructura del proyecto.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App(): React.JSX.Element {
+  return (
+    <main className="app">
+      <h1>Control Territorial</h1>
+    </main>
+  )
+}
+
+export default App
